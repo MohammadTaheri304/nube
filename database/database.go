3c34450b95586df5dc389d39809c185e8157b02a
Mohammad Taheri <mohammadtaheri304@gmail.com>
2019-10-29 11:47:47 +0330

    dd recovery function

--- database/database.go
+++ database/database.go
@@ -1,9 +1,12 @@
 package database
 
 import (
+	"bufio"
+	"io"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -23,12 +26,44 @@ func NewDatabase() *Database {
 		version: 0,
 	}
 
+	db.recovery()
 	go db.fileHandler()
 	return db
 }
 
+func (db *Database) recovery() {
+	f, err := os.Open("changelog")
+	if err != nil {
+		log.Printf("Error in opening file for recovery %+v", err)
+		f, err = os.Create("changelog")
+		if err != nil {
+			log.Fatalf("Error in recovery (create recovery file): %+v", err)
+		}
+	}
+	defer f.Close()
+
+	reader := bufio.NewReader(f)
+
+	for {
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+
+		args := strings.Split(string(line), " ")
+		version, _ := strconv.ParseInt(args[0], 16, 64)
+		//operation := args[1]
+		key := args[2]
+		value := args[3]
+
+		db.version = version
+		db.data.Store(key, value)
+	}
+	log.Println("Recovery done. Latest version is " + strconv.FormatInt(db.version, 16))
+}
+
 func (db *Database) fileHandler() {
-	f, err := os.Create("changelog")
+	f, err := os.OpenFile("changelog", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatalf("Error in opening file %+v", err)
 	}
